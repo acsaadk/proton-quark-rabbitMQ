7f7bf19e3d94f01ae0df30a3f5560dc2b7362cbf
Antonio Saad <antoniosaadkhalil@gmail.com>
2016-04-20 16:36:00 -0430

    PubSub Object implemented and tested

--- pubsub.js
+++ pubsub.js
@@ -1,8 +1,54 @@
 'use strict'
 
-class PubSub {
-  constructor(){
+const amqplib = require('amqplib');
 
+class PubSub {
+
+  constructor(){
+    this._rx = null;
+    this._tx = null;
+  }
+
+  connect(url){
+    return this.connectForRx(url).connectForTx(this._rx)
+  }
+
+  connectForRx(url){
+    this._rx = (typeof url === 'string') ? amqplib.connect(url) : url
+    return this
+  }
+
+  connectForTx(url){
+    this._tx = (typeof url === 'string') ? amqplib.connect(url) : url
+    return this
+  }
+
+  /**
+   * @method publish
+   * @description Publish a message to a specific queue
+   * @author Antonio Saad
+   */
+  publish(queue, msg, opts){
+    if(this._tx === null) throw new Error("No connection available for publishing")
+    return this._tx.then(conn => conn.createChannel())
+    .then(channel => [channel, channel.assertQueue(queue)])
+    .then(results => results[0].sendToQueue(queue, new Buffer(JSON.stringify({data: msg})), opts)) // results[0] is the channel
+  }
+
+  subscribe(queue, opts, noAck){
+    if(this._rx === null) throw new Error("No connection available for consuming")
+    return this._rx.then(conn => conn.createChannel())
+    .then(channel => Promise.all([channel, channel.assertQueue(queue)]))
+    .then(results => {
+      const channel = results[0]
+      return new Promise((resolve, reject) => {
+        channel.consume(queue, msg => {
+          if(!noAck) channel.ack(msg)
+          msg.content = JSON.parse(msg.content.toString()).data
+          return (msg) ? resolve(msg) : reject(new Error("Message is null"))
+        }, opts)
+      })
+    })
   }
 }
 
--- test/quark.js
+++ test/quark.js
@@ -1,2 +1,31 @@
-const Quark = require('../index');
+const PubSub = require('../pubsub');
 const should = require('should');
+
+const URL = 'amqp://MWrhIIqz:F0b_qvFT5JURJgbcylnz7rQqYlZJrkWo@hiding-threarah-45.bigwig.lshift.net:11043/_g_VaWPW3DRC';
+
+describe('PubSub class test', () => {
+
+  const pubsub = new PubSub()
+  pubsub.connect(URL)
+
+  it('should subscribe to a specific queue', done => {
+    pubsub
+    .subscribe('test_queue')
+    .then(msg => {
+      console.log(msg.content)
+      done()
+    })
+    .catch(err => {
+      done(err)
+    })
+  })
+  
+  it('should publish a message to a specific queue', done => {
+    pubsub
+    .publish('test_queue', {id: 2435, desc: 'This is a test message published in test_queue'})
+    .then(result => {
+      console.log(result)
+      done()
+    })
+  })
+})
