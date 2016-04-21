55f7b9bdb35c44c0df81f2963d7e38822a0a9a0c
Antonio Saad <antoniosaadkhalil@gmail.com>
2016-04-20 20:26:53 -0430

    Comments added in test

--- test/quark.js
+++ test/quark.js
@@ -8,6 +8,7 @@ describe('PubSub class test', () => {
   const pubsub = new PubSub()
   pubsub.connect(URL)
 
+  // Consumer
   it('should subscribe to a specific queue', done => {
     pubsub
     .subscribe('test_queue')
@@ -20,6 +21,7 @@ describe('PubSub class test', () => {
     })
   })
 
+  // Publisher
   it('should publish a message to a specific queue', done => {
     pubsub
     .publish('test_queue', {id: 2435, desc: 'This is a test message published in test_queue'})
