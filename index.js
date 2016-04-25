e8d2d01b3134504e6b1b0d723945bdff815a6ec1
Antonio Saad <antoniosaadkhalil@gmail.com>
2016-04-25 16:10:52 -0430

    Quark implementation

--- index.js
+++ index.js
@@ -3,18 +3,27 @@
 const Quark = require('proton-quark');
 const PubSub = require('./pubsub');
 
+const TX = process.env.AMQP_URL_TX || process.env.AMQP_URL || null
+const RX = process.env.AMQP_URL_RX || process.env.AMQP_URL || null
+
 class AmqpPubSubQuark extends Quark {
 
   constructor(proton){
-
+    super(proton)
   }
 
   validate(){
-
+    if(!TX) {
+      throw new Error("AMQP URL for transmition is missing")
+    }
+    if(!RX) {
+      throw new Error("AMQP URL for reception is missing")
+    }
   }
 
   initialize(){
-
+    const pubsub = new PubSub()
+    global.pubSub = pubsub.connectForRx(RX).connectForTx(TX)
   }
 
 }
